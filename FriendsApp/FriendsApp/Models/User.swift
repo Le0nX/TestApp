a106a741c366c492b76b667ed0413ed185743ff9
Le0nX <cloudmindone@gmail.com>
2019-11-09 22:07:57 +0300

    feature: user model + WebService twicks

---
+++ FriendsApp/FriendsApp/Models/User.swift
@@ -0,0 +1,50 @@
+//
+//  File.swift
+//  FriendsApp
+//
+//  Created by Denis Nefedov on 09.11.2019.
+//  Copyright © 2019 X. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    let id: Int
+    let firstName: String
+    let lastName: String
+    let email: String
+    let imageUrl: String?
+    let dateCreated: Date?
+    
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case firstName = "first_name"
+        case lastName = "last_name"
+        case email
+        case imageUrl = "avatar_url"
+        case dateCreated = "created_at"
+    }
+}
+
+
+extension User {
+//    init(from decoder: Decoder) throws {
+//        let container = try decoder.container(keyedBy: CodingKeys.self)
+//        id = try container.decode(Int.self, forKey: .id)
+//        firstName = try container.decode(String.self, forKey: .firstName)
+//        lastName = try container.decode(String.self, forKey: .lastName)
+//        email = try container.decode(String.self, forKey: .email)
+//        imageUrl = try container.decode(String.self, forKey: .imageUrl)
+//        dateCreated = try container.decode(String.self, forKey: .dateCreated)
+//    }
+    
+    // пропускаем dateCreated поле для POST и PATCH запросов
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(firstName, forKey: .firstName)
+        try container.encode(lastName, forKey: .lastName)
+        try container.encode(email, forKey: .email)
+        try container.encode(imageUrl, forKey: .imageUrl)
+    }
+}
--- FriendsApp/FriendsApp/Services/WebService.swift
+++ FriendsApp/FriendsApp/Services/WebService.swift
@@ -8,13 +8,53 @@
 
 import Foundation
 
+
+var decoder = JSONDecoder()
+//decoder.dateDecodingStrategy = .iso8601
+
+let baseUrl = URL(string: "https://frogogo-test.herokuapp.com/")
+
+
+/**
+    Может принимать в себя различные дженерик данные
+ для методов PATCH и POST
+ */
+enum HttpMethod<Body> {
+    case get
+    case patch(Body)
+    case post(Body)
+}
+
+extension HttpMethod {
+    var method: String {
+        switch self {
+        case .get: return "GET"
+        case .patch: return "PATCH"
+        case .post: return "POST"
+        }
+    }
+}
+
 /**
    Структура запрашиваемых данных вместе с обработчиком
        Воозвращает нам уже обработанные данные или nil
 */
 struct Resource<T> {
     let url: URL
+    let method: HttpMethod<Data>
     let parse: (Data) -> T?
+}
+
+extension Resource where T: Decodable {
+    init<Body: Encodable> (url: URL, method: HttpMethod<Body>) {
+        self.url = url
+        self.method = method.map { value in
+            try! JSONEncoder().encode(value)
+        }
+        self.parse = { data in
+            try? JSONDecoder().decode(T.self, from: data)
+        }
+    }
 }
 
 final class WebService {
@@ -23,14 +63,11 @@ final class WebService {
     func load<T>(_ resource: Resource<T>, completion: @escaping (T?) -> (Void)) {
         URLSession.shared.dataTask(with: resource.url, completionHandler: { data, response, error in
             
-            if let data = data {
-                DispatchQueue.main.async {
-                    completion(resource.parse(data))
-                }
-            } else {
-                completion(nil)
+            DispatchQueue.main.async {
+                completion(data.flatMap(resource.parse))
             }
             
         }).resume()
     }
 }
+
